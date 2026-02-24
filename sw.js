7980f44f1a0bd4ac126ab1716516e8ef7e7ee6c6
yovvani97 <yovvani97@gmail.com>
2026-02-24 13:50:24 -0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tonalpohualli-v1';
+const assets = [
+  './',
+  './index.html',
+  './nemontemi.html',
+  './js/lucide.min.js',
+  './logo.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
