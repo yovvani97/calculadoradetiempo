a9f9cd6ce4c03dee5d6864cc6123539893f2a63a
yovvani97 <yovvani97@gmail.com>
2026-02-24 18:33:56 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,28 @@
-const CACHE_NAME = 'tonalpohualli-v5'; // Subimos a v5 por el cambio de dominio
+const CACHE_NAME = 'tonalpohualli-v6'; // Subimos a v6 para forzar la actualización
 const assets = [
-  '/',
-  '/index.html',
-  '/nemontemi.html',
-  '/js/lucide.min.js',
-  '/logo.png',
-  '/manifest.json',
+  './',
+  './index.html',
+  './nemontemi.html',
+  './logo.png',
+  './manifest.json',
+  './signos/spinner-grecas.png', // IMPORTANTE: Añadimos la greca
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js'
 ];
 
-// Instalación: Guarda los archivos en el caché del celular
+// Instalación
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Fuerza al SW nuevo a tomar el control de inmediato
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return Promise.all(
-        assets.map(url => {
-          return cache.add(url).catch(err => console.log('Error cargando:', url, err));
-        })
-      );
+      return cache.addAll(assets);
     })
   );
 });
 
-// Activación: Limpia cachés antiguos de la dirección anterior
+// Activación
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -36,7 +33,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Interceptor: Sirve los archivos desde el caché si no hay internet
+// Interceptor
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
