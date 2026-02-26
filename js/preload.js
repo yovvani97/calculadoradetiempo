995d7f6e9c84490e4fe431b461925d3aff94e1ef
yovvani97 <yovvani97@gmail.com>
2026-02-25 18:14:05 -0600

    Create preload.js

---
+++ js/preload.js
@@ -0,0 +1,22 @@
+// Precarga silenciosa de los glifos
+export function preloadSignos() {
+  const total = 20;
+  let loaded = 0;
+
+  return new Promise((resolve) => {
+    for (let i = 1; i <= total; i++) {
+      const img = new Image();
+      img.src = `signos/${i}.png`;
+
+      img.onload = () => {
+        loaded++;
+        if (loaded === total) resolve();
+      };
+
+      img.onerror = () => {
+        loaded++;
+        if (loaded === total) resolve();
+      };
+    }
+  });
+}
